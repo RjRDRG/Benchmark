c1905b6ba2e71821976e3ef87b2c73b90cdc9ed7
Rodrigo Ribeiro <jr.rodrigo.pt@gmail.com>
2022-09-09 19:08:41 +0100

    wip

--- Setup/fowardPorts.sh
+++ Setup/fowardPorts.sh
@@ -4,3 +4,5 @@ kubectl --namespace monitoring port-forward svc/prometheus-grafana 3000:80
 kubectl --namespace monitoring port-forward svc/prometheus-pushgateway 9091
 
 kubectl --namespace monitoring port-forward svc/prometheus-kube-prometheus-prometheus 9090
+
+kubectl --namespace apps port-forward svc/entry 4004:80
