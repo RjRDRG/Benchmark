54c9a64776d7cbff33a5fb2ab3768c11719edc3a
Rodrigo Ribeiro <jr.rodrigo.pt@gmail.com>
2022-09-11 14:40:06 +0100

    wip

--- setup/up.sh
+++ setup/up.sh
@@ -10,7 +10,7 @@ helm repo update
 
 helm install prometheus-pushgateway --atomic prometheus-community/prometheus-pushgateway --namespace monitoring
 
-helm install prometheus --atomic prometheus-community/kube-prometheus-stack --namespace monitoring --values pushgateway_values.yaml
+helm install prometheus --atomic prometheus-community/kube-prometheus-stack --namespace monitoring --values prometheus_values.yaml
 
 cd artillery-operator
 
@@ -18,7 +18,7 @@ source ./operator-deploy.sh
 
 kubectl -n artillery-operator-system set env deployments/artillery-operator-controller-manager ARTILLERY_DISABLE_TELEMETRY=true
 
-sleep 30
+kubectl wait deployment -n artillery-operator-system artillery-operator-controller-manager --for condition=Available=True --timeout=30s
 
 kubectl create clusterrolebinding artillery-cluster-admin --clusterrole=cluster-admin --serviceaccount=artillery-operator-system:artillery-operator-controller-manager
 
