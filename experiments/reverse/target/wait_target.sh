9bcbe23f36546a87a2fd5405d429ae8c135b65e6
Rodrigo Ribeiro <jr.rodrigo.pt@gmail.com>
2022-09-12 13:13:30 +0100

    wip

--- experiments/reverse/target/wait_target.sh
+++ experiments/reverse/target/wait_target.sh
@@ -2,7 +2,7 @@
 
 set -o errexit
 
-kubectl wait deployment -n apps app1 --for condition=Available=True --timeout=300s
-kubectl wait deployment -n apps app2 --for condition=Available=True --timeout=300s
-kubectl wait deployment -n apps app3 --for condition=Available=True --timeout=300s
-kubectl wait deployment -n apps app4 --for condition=Available=True --timeout=300s
+kubectl wait deployment -n {{ my_test_job_name }} app1 --for condition=Available=True --timeout=300s
+kubectl wait deployment -n {{ my_test_job_name }} app2 --for condition=Available=True --timeout=300s
+kubectl wait deployment -n {{ my_test_job_name }} app3 --for condition=Available=True --timeout=300s
+kubectl wait deployment -n {{ my_test_job_name }} app4 --for condition=Available=True --timeout=300s
